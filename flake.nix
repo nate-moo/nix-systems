a841fcd3716498574b125353ab2f8cb935c97489
nathan <nathan-moore1@outlook.com>
2025-09-03 13:31:01 -0600

    update and add sops-nix

--- flake.nix
+++ flake.nix
@@ -6,15 +6,21 @@
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     quickshell.url = "github:quickshell-mirror/quickshell";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, quickshell, nixos-hardware, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-stable, quickshell, nixos-hardware, sops-nix, ... }@inputs: {
     # Desktop
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./desktop/configuration.nix
+
+	#./common/common.nix
+
+	sops-nix.nixosModules.sops
       ];
     };
 
@@ -24,10 +30,15 @@
     specialArgs = { inherit inputs; };
     modules = [
       ./lappy/configuration.nix
+
+      #./common/common.nix
+
       nixos-hardware.nixosModules.common-cpu-intel
       nixos-hardware.nixosModules.common-hidpi
       nixos-hardware.nixosModules.common-pc-laptop-ssd
       nixos-hardware.nixosModules.common-pc-laptop
+
+      sops-nix.nixosModules.sops
       ];
     };
   };
