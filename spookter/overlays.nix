ad7e1f2df212d5e005f373fa776f44c3e6f358d9
Nate <nathan-moore1@outlook.com>
2026-06-05 15:08:45 -0600

    overlay

---
+++ spookter/overlays.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }:
+
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      qdmr = prev.qdmr.overrideAttrs (old: {
+          pname = "qdmr";
+          version = "0.15.0";
+          src = prev.fetchFromGitHub {
+            owner = "hmatuschek";
+            repo = "qdmr";
+            rev = "3db0b2f4ade5e6c2a818ecc6f410f0a19087b32e";
+            hash = "sha256-OPoJ9N2j8wlBQUfj8CRiyR80zAFPdYR6xm56sesL/D0=";
+          };
+          buildInputs = (old.buildInputs or []) ++ [ final.qt6.qtmultimedia ];
+          postPatch = builtins.replaceStrings ["--replace"] ["--replace-fail"] (old.postPatch or "");
+        });
+      })
+  ];
+}
